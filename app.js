63599b43ec956e52e32f78a3950bc29003d88714
geekabhinav007 <abhinav26january@gmail.com>
2023-05-19 01:03:48 +0530

    ejs working done

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
+const path = require('path')
 
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
 app.get('/', (req, res) => {
-    res.send('Hello I am abhinav')
+    res.render('home')
 })
 
 app.listen(3000, ()=> {
