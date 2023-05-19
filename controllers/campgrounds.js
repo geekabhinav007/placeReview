cbb44823a0379874b955285639f7754a88f63840
geekabhinav007 <abhinav26january@gmail.com>
2023-05-19 11:38:41 +0530

    created Pace Review

---
+++ controllers/campgrounds.js
@@ -0,0 +1,26 @@
+const Campground = require('../models/campground');
+
+
+module.exports.index = async (req, res) => {
+    const campgrounds = await Campground.find({}).populate('popupText');
+    res.render('campgrounds/index', { campgrounds })
+}
+
+module.exports.renderNewForm = (req, res) => {
+    res.render('campgrounds/new');
+}
+
+module.exports.createCampground = async (req, res, next) => {
+    const geoData = await geocoder.forwardGeocode({
+        query: req.body.campground.location,
+        limit: 1
+    }).send()
+    const campground = new Campground(req.body.campground);
+    campground.geometry = geoData.body.features[0].geometry;
+    campground.images = req.files.map(f => ({ url: f.path, filename: f.filename }));
+    campground.author = req.user._id;
+    await campground.save();
+    console.log(campground);
+    req.flash('success', 'Successfully made a new Place Review!');
+    res.redirect(`/campgrounds/${campground._id}`)
+}
